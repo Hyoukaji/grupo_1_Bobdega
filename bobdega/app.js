485db7cbeb2c3b5f91dfab0b385028a35f81bc4e
alejoAliberti <alibertialejo56@gmail.com>
2021-12-12 18:06:50 -0300

    actualizacion header

--- bobdega/app.js
+++ bobdega/app.js
@@ -11,3 +11,20 @@ app.listen(3000,()=>
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"))
 });
+
+app.get('/signin', (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/signin.html"))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+});
+
+app.get('/shoppingCart', (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/shoppingCart.html"))
+});
+
+app.get('/Product', (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/product.html"))
+});
+
