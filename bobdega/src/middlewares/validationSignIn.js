07cee8c54a4661269726d446eaf8acd023124967
alejoAliberti <alibertialejo56@gmail.com>
2022-04-03 18:32:29 -0300

    cambios

--- bobdega/src/middlewares/validationSignIn.js
+++ bobdega/src/middlewares/validationSignIn.js
@@ -1,38 +1,27 @@
 const path = require('path')
 const { body } = require('express-validator');
-const { User } = require("../../database/models");
+
 
 validationSignIn =  [
 
     body('firstName')
-        .notEmpty().withMessage('completar Nombre').bail()
-        .isLength({ min:2, max:20 }).withMessage("Minimo 2 caracteres y maximo 20"),
+        .notEmpty().withMessage('completar Nombre').bail(),
 
     body('email')
 		.notEmpty().withMessage('Tienes que escribir un correo electrónico').bail()
-		.isEmail().withMessage('Debes escribir un formato de correo válido')
-        .custom(async (email) => {
-            const existingUser = await User.findOne({
-                where: { 
-                    email: email
-                }
-            });
-            if (existingUser) {
-                throw new Error('Email ya registrado')
-            }
-        }),
-    
+		.isEmail().withMessage('Debes escribir un formato de correo válido'),
+
     body('lastName')
-        .notEmpty().withMessage('Completar Apellido').bail()
-        .isLength({ min:2, max:20 }).withMessage("Minimo 2 caracteres y maximo 20"),
+        .notEmpty().withMessage('Completar Apellido').bail(),
+
+
 
     body('username')
         .notEmpty().withMessage('Completar ususario').bail(),
 
+    
 	body('password')
 		.notEmpty().withMessage('Tienes que escribir una contraseña').bail()
-        .isLength({ min:8, max:30 }).withMessage("Debe tener al menos 8 caracteres").bail()
-        .matches(/^(?=.\d)(?=.[a-z])(?=.[A-Z])[a-zA-Z\d@$.!%#?&]/,).withMessage("Tiene que tener una mayuscula, una minuscula y un caracter especial")
 		.custom((value, { req }) => {
 			if (value !== req.body.password2) {
 				throw new Error('Las contraseñas tienen que coincidir');
