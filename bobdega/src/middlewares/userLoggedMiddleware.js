79140112118babdcbe8077bf3d39bc84ba1d94b3
Hyoukaji <florespedromartin@gmail.com>
2022-04-17 16:28:43 -0300

    Permisos de admin

--- bobdega/src/middlewares/userLoggedMiddleware.js
+++ bobdega/src/middlewares/userLoggedMiddleware.js
@@ -1,10 +1,14 @@
 function userLoggedMiddleware (req, res, next) {
 	res.locals.isAnUserLogged = false;
+	res.locals.isAnAdminLogged = false;
 
 	if (req.session.userLogged !== undefined) {
 		res.locals.isAnUserLogged = true;
 		res.locals.userData = {
 			name: req.session.userLogged.firstName,			
+		}
+		if (req.session.userLogged.category == "Admin"){
+			res.locals.isAnAdminLogged = true;
 		}
 	}
 
