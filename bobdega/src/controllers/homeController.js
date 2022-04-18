7e3b02fd0be01b41116654b408d69f420ad467c9
alejoAliberti <alibertialejo56@gmail.com>
2022-04-17 21:15:25 -0300

    finalizando estilos

--- bobdega/src/controllers/homeController.js
+++ bobdega/src/controllers/homeController.js
@@ -8,11 +8,21 @@ const controller = {
     home : async (req,res)=>{
         const productsGo = await Product.findAll()
         let products = []
+        let gift = []
+        let tintos = []
+        let blancos = []
         for (let i = 0; i < productsGo.length; i++){
             products.push(productsGo[i].dataValues)
+            if (productsGo[i].dataValues.typeId == 5){
+                gift.push(productsGo[i].dataValues)
+            }else if(productsGo[i].dataValues.typeId == 2){
+                tintos.push(productsGo[i].dataValues)
+            }else if(productsGo[i].dataValues.typeId == 3){
+                blancos.push(productsGo[i].dataValues)
+            }
         }
         return res.render(
-            'home',{products}
+            'home',{products,gift,tintos,blancos}
         )
     },
 }
