182564e4858569c6d331c58dbb42cb34b9e6b9df
alejoAliberti <alibertialejo56@gmail.com>
2022-04-17 21:27:38 -0300

    coma de m

--- bobdega/docs/bobdega.sql
+++ bobdega/docs/bobdega.sql
@@ -22,7 +22,7 @@ SET time_zone = "+00:00";
 --
     DROP DATABASE IF EXISTS bobdega;
     CREATE DATABASE bobdega;
-    USE bobdega
+    USE bobdega;
 -- --------------------------------------------------------
 
 --
