a053fbf271ca7f3b148f37f4ff7fc505a88f5f07
alejoAliberti <alibertialejo56@gmail.com>
2022-04-19 23:54:55 -0300

    arreglando cookies

--- bobdega/src/middlewares/userLoggedMiddleware.js
+++ bobdega/src/middlewares/userLoggedMiddleware.js
@@ -8,30 +8,34 @@ async function userLoggedMiddleware  (req, res, next) {
 	console.log("aca la cookie")
 	console.log(req.cookies)
 	
-	if (req.cookies && req.cookies.usuario){
-		const usuarioGo = await User.findOne({where :{email : req.cookies.usuario}})
-		console.log(usuarioGo.dataValues)
-		req.session.userLogged = usuarioGo.dataValues;
+	// if (req.cookies && req.cookies.usuario){
+	// 	const usuarioGo = await User.findOne({where :{email : req.cookies.usuario}})
+	// 	console.log(usuarioGo.dataValues)
+	// 	req.session.userLogged = usuarioGo.dataValues;
 
-	}
+	// }
 
-	if (req.session.userLogged !== undefined) {
+	if (req.session.userLogged !== undefined) { /**vemos si hay alguien en session */
 		res.locals.isAnUserLogged = true;
 		res.locals.userData = {
-			name: req.session.userLogged.firstName,			  //valida usuario logueado
+			name: req.session.userLogged.firstName,			  //name mostrado en el home
 		}
-		if (req.session.userLogged.category == "Admin"){
+		if (req.session.userLogged.category == "Admin"){/**vemos si es un administrador */
 			res.locals.isAnAdminLogged = true;
 		}
-	}//else if(usuarioGo){
-	// 	res.locals.isAnUserLogged = true;
-	// 	res.locals.userData = {
-	// 		name: req.session.userLogged.firstName,			  //valida usuario logueado
-	// 	}
-	// 	if (req.session.userLogged.category == "Admin"){
-	// 		res.locals.isAnAdminLogged = true;
-	// 	}
-	// }
+	}else if(req.cookies && req.cookies.usuario){/**en caso de no haber nadie en session verificamos la cookie */
+		console.log("entre en el elseif")
+	 	const usuarioGo = await User.findOne({where :{email : req.cookies.usuario}})	//buscamos el usuario en la base de datos con su email
+		console.log(usuarioGo.dataValues)
+		req.session.userLogged = usuarioGo.dataValues;		/**cargamos a la persona en el session */
+		res.locals.isAnUserLogged = true;
+		res.locals.userData = {
+			name: req.session.userLogged.firstName,			  //name mostrado en el home
+		}
+		if (req.session.userLogged.category == "Admin"){	/**verificamos si es un administrador */
+			res.locals.isAnAdminLogged = true;
+		}
+	 }
 
 	next();
 }
